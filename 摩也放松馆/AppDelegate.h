ffd1fca532f15a1a97d25b90729637e902479c64
zhangxiaodao <1547100913@qq.com>
2017-07-05 15:51:08 +0800

    Initial Commit

---
+++ 摩也放松馆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  摩也放松馆
+//
+//  Created by 杭州阿尔法特 on 2017/7/5.
+//  Copyright © 2017年 张海昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 摩也放松馆/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  摩也放松馆
+//
+//  Created by 杭州阿尔法特 on 2017/7/5.
+//  Copyright © 2017年 张海昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
